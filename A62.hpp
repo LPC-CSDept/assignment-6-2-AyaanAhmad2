9f0ecef0d3c732d8b2a1ef37c58722513e7f21be
Ayaan Ahmad <ayanahmad175@gmail.com>
2022-10-04 18:26:29 -0700

    kablooey

--- A62.hpp
+++ A62.hpp
@@ -1,16 +1,19 @@
 //
 
-#include <iomanip>
 #include <iostream>
+#include <iomanip>
+#include <stdio.h>      /* printf */
+#include <math.h>
 using namespace std;
 
 // complete two functions
 double nthpowerp(int n)
 {
-        // return n_th power number
+        return (pow (2, n));
 }
 double nthpowern(int n)
 {
+        return (pow (2, n));
         // return n_th power number
 }
 void printout(double result)
